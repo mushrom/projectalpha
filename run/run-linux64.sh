e5c5702f564b814f0e60157dea8505de4df608bf
My Name <foo@lisp.us.to>
2021-07-19 03:13:44 -0400

    Passing command line arguments from launcher script

--- run/run-linux64.sh
+++ run/run-linux64.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname $0)"
-LD_LIBRARY_PATH="$PWD/lib" ./lib/ld-linux-x86-64.so.2 ./bin/landscape-demo
+LD_LIBRARY_PATH="$PWD/lib" ./lib/ld-linux-x86-64.so.2 ./bin/landscape-demo $@
