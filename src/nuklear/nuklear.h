8cbb01385036bea673ca0069c45c47215ea266b4
My Name <foo@lisp.us.to>
2021-10-24 00:58:02 -0400

    Adding function from nuklear to load images

--- src/nuklear/nuklear.h
+++ src/nuklear/nuklear.h
@@ -11,3 +11,32 @@
 
 #include "impl/nuklear.h"
 #include "impl/nuklear_sdl_gl3.h"
+
+#include <stb/stb_image.h>
+// icon_load() function from nuklear extended demo
+static inline struct nk_image
+nk_image_load(const char *filename)
+{
+    int x,y,n;
+    GLuint tex;
+
+    unsigned char *data = stbi_load(filename, &x, &y, &n, 0);
+    //if (!data) die("[SDL]: failed to load image: %s", filename);
+    if (!data) {
+		exit(1);
+		fprintf(stderr, "couldn't load image %s\n", filename);
+		return nk_image_id(0);
+	}
+
+    glGenTextures(1, &tex);
+    glBindTexture(GL_TEXTURE_2D, tex);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
+
+    return nk_image_id((int)tex);
+}
